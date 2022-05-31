cf04a69d864780166b82e626cd50bad632726978
yongchanson <syc9430@gmail.com>
2022-06-01 04:44:46 +0900

    etc:디자인적용을 위해 code클레스 부모요소에 wrapCode클레스 추가, file-list의 자식클레스에 levelTtile클레스 추가

--- Section/SearchResult/components/index.js
+++ Section/SearchResult/components/index.js
@@ -12,7 +12,7 @@ export default function SearchResult({ level, fileName }) {
         <button class="btnNextSolution">다음 해설</button>
       </div>
       <div class="file-title"></div>
-      <div>
+      <div class="wrapCode">
         <pre class="code"></pre>
         <button class="btn-copy">코드 복사하기</button><span class="isCopied"></span>
       </div>
