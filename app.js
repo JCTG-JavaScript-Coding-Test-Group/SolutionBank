60ecad21ac77164743ae0ba4f0e90cbec4ea78d1
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-06-06 16:38:02 +0900

    feat: 페이지 분리 없이 로딩 기능 구현

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import SearchBox from './Section/SearchBox/components/index.js';
 
 const $app = document.querySelector('.app');
+const $loading = document.querySelector('.loading');
 
 const init = async () => {
   $app.innerHTML = `
@@ -9,6 +10,16 @@ const init = async () => {
   `;
   const $searchBox = new SearchBox();
   $searchBox.render();
+};
+
+const loading = async () => {
+  $loading.innerHTML = `
+  <img src="images/spinner.svg">
+
+  Loading…
+  `;
+  await init();
+  $loading.style.display = 'none';
 };
 
 init();
