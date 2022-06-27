0efdb02bf9d549c3456abaf8b7f9b0c9cbd0c6be
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-06-27 21:39:14 +0900

    refactor: 필요 없는 import 제거

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot, atom } from 'recoil';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
